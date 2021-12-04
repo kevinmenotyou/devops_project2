a2c47436f6b33b7580fccdb73ec9e878ebd432fc
Kevin Hutchison <khutchison08@gmail.com>
2021-12-04 13:20:31 -0500

    Fixing terraform script

--- terraform/local_registry_dependent/variables.tf
+++ terraform/local_registry_dependent/variables.tf
@@ -1,9 +1,3 @@
-variable "registry_container_name" {
-  description = "Value of the name for the Docker Registry"
-  type        = string
-  default     = "LocalDockerRegistry"
-}
-
 variable "devops_diagram_container_name" {
   description = "Value of the name for the DevOps Diagram"
   type        = string
